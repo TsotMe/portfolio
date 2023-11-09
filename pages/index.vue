a1717175b5f3c2f6431878dd7211ff387c85f849
Tsotne <cotnemexuri@gmail.com>
2023-11-09 10:52:51 +0400

    Add base css reset styles

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,9 @@
 @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;500&display=swap');
 
 * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
     font-family: 'Rubik', sans-serif;
 }
 
