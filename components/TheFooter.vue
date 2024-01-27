d814a41df5ac3777897132755817994ab027305e
Tsotne <cotnemexuri@gmail.com>
2024-01-27 20:15:35 +0400

    Add initial header and footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<footer>
+  <p>© 2023-2024 TM. All rights reserved</p>
+</footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  height: 76px;
+  display: flex;
+  align-items: center;
+
+  p {
+    //color: #657586;
+    font-weight: 400;
+  }
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header>
+  <ul>
+    <li>Overview</li>
+    <li>Projects</li>
+  </ul>
+</header>
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  height: 76px;
+  border-bottom: 1px solid rgba(217, 217, 217, 0.78);
+  background: #FFF;
+
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 16px;
+
+    li {
+      color: #657586;
+      font-weight: 400;
+      cursor: pointer;
+      user-select: none;
+    }
+  }
+}
+</style>
