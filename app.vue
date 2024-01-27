744afb2005d1832cf203ff5a854964482746b60f
Tsotne <cotnemexuri@gmail.com>
2024-01-27 20:16:30 +0400

    Create main layout

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="app">
+  <TheHeader/>
+  <NuxtPage/>
+  <TheFooter/>
+</div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100dvh;
+
+  .main-content {
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,48 +3,38 @@
 </script>
 
 <template>
-    <div class="home-page">
-        <h1>Hello.</h1>
-        <p class="name">My name is Tsotne Meskhuri</p>
-        <p>I'm web developer currently based in Georgia.</p>
-<!--        Results-driven Front-End Developer with more than 2 years of experience translating design concepts into-->
-<!--        responsive and intuitive user interfaces. Proficient in HTML, CSS, JavaScript, and modern front-end-->
-<!--        frameworks. Adept at collaborating with cross-functional teams to deliver high-quality web applications-->
-<!--        that enhance user experiences. Skilled in optimizing website performance and maintaining code integrity.-->
-<!--        Committed to staying updated with industry trends and delivering innovative solutions.-->
-    </div>
+<div class="home-page">
+  <h1>Hello.</h1>
+  <p class="name">My name is Tsotne Meskhuri</p>
+  <p>I'm web developer currently based in Georgia. I have 3 years of
+    experience translating design concepts into
+    responsive and intuitive user interfaces. Proficient in HTML, CSS, JavaScript, and modern front-end
+    frameworks. Adept at collaborating with cross-functional teams to deliver high-quality web applications
+    that enhance user experiences. Skilled in optimizing website performance and maintaining code integrity.
+    Committed to staying updated with industry trends and delivering innovative solutions.</p>
+</div>
 </template>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;500&display=swap');
-
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Rubik', sans-serif;
-}
-
 .home-page {
-    padding: 5vh 10vw;
+  margin: 40px 0;
 
-    h1 {
-        font-weight: 500;
-        font-size: 4.5rem;
-        margin-top: 40px;
-    }
+  h1 {
+    font-weight: 500;
+    font-size: 4.5rem;
+  }
 
-    p.name {
-        font-weight: 500;
-        font-size: 2.2rem;
-        margin: 30px 0;
-    }
+  p.name {
+    font-weight: 500;
+    font-size: 2.2rem;
+    margin: 30px 0;
+  }
 
-    p {
-        font-size: 1.6em;
-        font-weight: 300;
-        line-height: 1.4;
-        max-width: 28em;
-    }
+  p {
+    font-size: 1.6em;
+    font-weight: 300;
+    line-height: 1.4;
+    max-width: 28em;
+  }
 }
 </style>
