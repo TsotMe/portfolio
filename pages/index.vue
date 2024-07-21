8d493c7a92815704db2d98a4c8492d724e6c9d20
tsotne <tsotnemeskhuri@gmail.com>
2024-07-21 18:50:39 +0400

    Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-slate-50 font-sans dark:bg-black">
-    <div
-      class="h-screen my-0 bg-white ring-1 ring-zinc-100 px-10 py-8 md:mx-10 lg:mx-20 xl:max-w-[1120px] xl:mx-auto dark:bg-zinc-900 dark:ring-zinc-300/20"
-    >
-      <AppHeader />
+  <div class="h-full">
+    <h1 class="text-4xl mb-4 text-zinc-800 dark:text-zinc-100">Hello.</h1>
+    <h2 class="text-xl mb-4 text-zinc-800 dark:text-zinc-100">My name is Tsotne Meskhuri.</h2>
 
-      <div class="">
-        <h1 class="text-4xl mb-4 text-zinc-800 dark:text-zinc-100">Hello.</h1>
-        <h2 class="text-xl mb-4 text-zinc-800 dark:text-zinc-100">My name is Tsotne Meskhuri.</h2>
-
-        <p class="max-w-3xl text-base text-zinc-800 dark:text-zinc-100">
-          I am software developer currently based in Georgia. I have a passion for web development
-          and love to create web applications. I am always looking for new opportunities to learn
-          and grow as a developer. When I'm not immersed in code, you'll find me playing video
-          games, binge-watching my favorite series, or hitting the basketball court.
-        </p>
-      </div>
-      <!-- <a href="https://github.com/TsotMe"></a> -->
-    </div>
+    <p class="max-w-3xl text-base text-zinc-800 dark:text-zinc-100">
+      I am software developer currently based in Georgia. I have a passion for web development and
+      love to create web applications. I am always looking for new opportunities to learn and grow
+      as a developer. When I'm not immersed in code, you'll find me playing video games,
+      binge-watching my favorite series, or hitting the basketball court.
+    </p>
   </div>
 </template>
