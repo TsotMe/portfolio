13cebafa2bf24bbd97756bb6cb3c923c22bae39a
tsotne <tsotnemeskhuri@gmail.com>
2024-07-21 18:45:59 +0400

    Add AppFooter component

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="flex flex-col items-center gap-3 md:flex-row md:justify-between">
+    <Navigation />
+    <span class="text-sm text-zinc-600 dark:text-zinc-400 text-center"
+      >© 2024 Tsotne Meskhuri. All rights reserved.</span
+    >
+  </footer>
+</template>
