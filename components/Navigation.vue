5d8186b4fe05d4ee19f0ab5c22230fa8d0e921d9
tsotne <tsotnemeskhuri@gmail.com>
2024-07-21 18:44:45 +0400

    Add stadalone Navigation component

---
+++ components/Navigation.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav
+    class="flex items-center gap-4 no-underline text-sm text-zinc-800 dark:text-zinc-100 cursor-pointer"
+  >
+    <NuxtLink href="">About</NuxtLink>
+    <NuxtLink href="">Projects</NuxtLink>
+    <NuxtLink href="">Services</NuxtLink>
+    <NuxtLink href="">Contact</NuxtLink>
+  </nav>
+</template>
