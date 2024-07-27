7dc09a946f007fc3ab4753fcf6c9d31bb2daec9d
tsotne <tsotnemeskhuri@gmail.com>
2024-07-27 21:30:58 +0400

    Update About page

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,23 @@
 
 <template>
   <div class="h-full">
-    <h1 class="text-4xl mb-4 text-zinc-800 dark:text-zinc-100">Hello.</h1>
-    <h2 class="text-xl mb-4 text-zinc-800 dark:text-zinc-100">My name is Tsotne Meskhuri.</h2>
+    <h1 class="text-4xl mb-4 mt-6 font-medium text-zinc-800 dark:text-zinc-100">Hello.</h1>
+    <h2 class="text-2xl mb-4 text-zinc-800 dark:text-zinc-100">My name is Tsotne Meskhuri.</h2>
 
-    <p class="max-w-3xl mb-4 text-base text-zinc-800 dark:text-zinc-100">
-      I am software developer currently based in Georgia. I have a passion for web development and
-      love to create web applications. I am always looking for new opportunities to learn and grow
-      as a developer. When I'm not immersed in code, you'll find me playing video games,
-      binge-watching my favorite series, or hitting the basketball court.
+    <p class="max-w-2xl mb-4 text-base text-zinc-800 dark:text-zinc-100">
+      I am software developer currently based in Georgia. I am passionate about using programming
+      tools to help people solve their problems. By leveraging my technical skills, I can create
+      innovative solutions that streamline operations, improve efficiency, and drive growth for
+      businesses. My goal is to empower others to achieve their objectives through the power of
+      technology. I really enjoy with feel of creating something new and useful. I am always looking
+      for new opportunities to learn and grow as a developer.
     </p>
 
-    <SocialMediaLinks />
+    <p class="max-w-2xl mb-4 text-base text-zinc-800 dark:text-zinc-100">
+      When I'm not immersed in code, I enjoy playing basketball. I play every weekend at the park
+      near my house. Sometimes I play with my friends, sometimes with strangers. My friend got me
+      into it, because he loves sports. I didn't like it at first, because it was hard and lost all
+      the time (like programming), but now I enjoy it. I also like to read books and watch series.
+    </p>
   </div>
 </template>
