0a5ba1bd023904f08d8f18f7613c0539519cc958
tsotne <tsotnemeskhuri@gmail.com>
2024-07-27 21:32:31 +0400

    Update Projects page

---
+++ pages/projects.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1
+      class="max-w-[680px] text-balance text-4xl font-medium mb-4 mt-6 text-zinc-800 dark:text-zinc-100"
+    >
+      Projects I've worked on to make a difference in the world.
+    </h1>
+
+    <p class="max-w-2xl mb-4 text-base text-zinc-800 dark:text-zinc-100">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur atque similique delectus
+      recusandae in perferendis optio, facilis sed non explicabo. Odit fugit facilis, modi dolorem
+      ducimus voluptate omnis ipsa sapiente.
+    </p>
+  </div>
+</template>
