75d3fb5cc35909972b614974f25402295083d543
tsotne <tsotnemeskhuri@gmail.com>
2024-07-27 21:28:56 +0400

    Add "SocialMediaLinks" component instead of Navigation on AppFooter

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,7 +4,7 @@
   <footer
     class="flex flex-col items-center gap-3 md:flex-row md:justify-between border-t border-zinc-100 dark:border-zinc-700/40 pt-7"
   >
-    <Navigation />
+    <SocialMediaLinks />
     <span class="text-sm text-zinc-600 dark:text-zinc-400 text-center"
       >© 2024 Tsotne Meskhuri. All rights reserved.</span
     >
