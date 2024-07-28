98644d501145e34bf3557497dd758f42f53e0a40
tsotne <tsotnemeskhuri@gmail.com>
2024-07-28 13:36:50 +0400

    Update layout paddings for mobile devices

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="bg-slate-50 font-sans dark:bg-black">
     <div
-      class="h-screen my-0 bg-white flex flex-col gap-10 ring-1 ring-zinc-100 px-10 py-8 md:mx-10 lg:mx-20 xl:max-w-[1120px] xl:mx-auto dark:bg-zinc-900 dark:ring-zinc-300/20"
+      class="min-h-screen my-0 bg-white flex flex-col gap-10 ring-1 ring-zinc-100 px-4 py-4 md:px-10 md:py-8 md:mx-16 lg:mx-20 xl:max-w-[1120px] xl:mx-auto dark:bg-zinc-900 dark:ring-zinc-300/20"
     >
       <AppHeader />
 
