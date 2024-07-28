85183b38ae79e711350901e9ad3a03b29bd8b5be
tsotne <tsotnemeskhuri@gmail.com>
2024-07-28 13:35:22 +0400

    Update padding size on AppFooter

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
   <footer
-    class="flex flex-col items-center gap-3 md:flex-row md:justify-between border-t border-zinc-100 dark:border-zinc-700/40 pt-7"
+    class="flex flex-col items-center gap-3 py-7 md:pb-0 md:flex-row md:justify-between border-t border-zinc-100 dark:border-zinc-700/40"
   >
     <SocialMediaLinks />
     <span class="text-sm text-zinc-600 dark:text-zinc-400 text-center"
