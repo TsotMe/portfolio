e6ac47cb4dab2397469acea46f2682290877e59f
tsotne <tsotnemeskhuri@gmail.com>
2024-08-01 20:32:01 +0400

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/eslint'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/color-mode'],
   eslint: {}
 })
