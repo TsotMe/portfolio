593b32ef9e637835b5fd6f26d87ce7b562804c8b
tsotne <tsotnemeskhuri@gmail.com>
2024-08-15 22:23:12 +0400

    Turn off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/color-mode'],
-  eslint: {}
+  eslint: {},
+  ssr: false
 })
