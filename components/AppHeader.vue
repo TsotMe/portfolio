4cceb2d8a29673d3d82eef970203a1768b817c8b
tsotne <tsotnemeskhuri@gmail.com>
2024-08-16 13:43:11 +0400

    Try to fix parent node error

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,30 +2,30 @@
 import darkIcon from '/assets/images/dark-mode.png'
 import lightIcon from '/assets/images/light-mode.png'
 
-// const colorMode = useColorMode()
+const colorMode = useColorMode()
 
-// const handleModeChange = () => {
-//   if (colorMode.preference === 'system') {
-//     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-//   } else {
-//     colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
-//   }
-// }
+const handleModeChange = () => {
+  if (colorMode.preference === 'system') {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  } else {
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+  }
+}
 
-// const modeButtonIconPath = computed(() => {
-//   if (colorMode.value === 'dark') {
-//     return darkIcon
-//   } else {
-//     return lightIcon
-//   }
-// })
+const modeButtonIconPath = computed(() => {
+  if (colorMode.value === 'dark') {
+    return darkIcon
+  } else {
+    return lightIcon
+  }
+})
 </script>
 
 <template>
   <header class="flex justify-between min-h-9">
     <Navigation />
 
-    <!-- <button
+    <button
       class="min-w-9 min-h-9 p-1.5 rounded-full ring-1 ring-zinc-100 dark:ring-zinc-300/20 dark:hover:ring-white/20"
       @click="handleModeChange"
     >
@@ -36,7 +36,7 @@ import lightIcon from '/assets/images/light-mode.png'
           <img :src="lightIcon" alt="Mode" loading="lazy" />
         </template>
       </ClientOnly>
-    </button> -->
+    </button>
   </header>
 </template>
 
