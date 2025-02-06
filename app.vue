55d0242f2cc720027dea86ddd6a37144e08357bb
tsotne <tsotnemeskhuri@gmail.com>
2025-02-06 12:53:21 +0400

    Add some meta information and lang attribute for html

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <script lang="ts" setup>
 useHead({
-  titleTemplate: 'TsotMe'
+  htmlAttrs: {
+    lang: 'en'
+  },
+  titleTemplate: 'TsotMe',
+  meta: [
+    { name: 'title', content: 'Tsotne Meskhuri | Portfolio' },
+    { name: 'ogTitle', content: 'Tsotne Meskhuri | Portfolio' },
+    {
+      name: 'description',
+      content:
+        "Explore the portfolio and work of Me. Learn more about my expertise, achievements, and the projects I'm passionate about."
+    },
+    {
+      name: 'ogDescription',
+      content:
+        "Explore the portfolio and work of Me. Learn more about my expertise, achievements, and the projects I'm passionate about."
+    }
+  ]
 })
 </script>
 
